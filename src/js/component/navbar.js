39423c31b4d216d34ecb6db1dc6c674cdc15ba3f
Carito 💫 <carito@Caritos-MacBook-Pro.local>
2023-01-10 23:39:42 -0300

    add navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,56 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Bienestar</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<nav className="navbar navbar-expand-lg bg-light">
+			<nav class="navbar bg-light">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#"><i class="fa-solid fa-person-biking"></i>Bienestar</a>
+				</div>
+			</nav>
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Recetas</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Deportes</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Tu semana</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Objetivos</a>
+					</li>
+				</ul>
+				<form class="d-flex" role="search">
+					<input
+						class="form-control me-2"
+						type="search"
+						placeholder="Search"
+						aria-label="Search"
+					/>
+					<button class="btn btn-outline-success" type="submit">Search</button>
+				</form>
+
+			{/* <!-- Setting botton --> */}
+				<li class="nav-item dropdown">
+					<a
+						class="nav-link dropdown-toggle"
+						href="#"
+						role="button"
+						data-bs-toggle="dropdown"
+						aria-expanded="false"
+					>
+						<i class="fa-solid fa-gear"></i>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a class="dropdown-item" href="#">Action</a></li>
+						<li><a class="dropdown-item" href="#">Another action</a></li>
+						<li><hr class="dropdown-divider" /></li>
+						<li>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</li>
+					</ul>
+				</li>
+			</nav>
 	);
 };
