58609890400309478352a2e1a3f28f7cd6f134c0
Carito 💫 <carito@Caritos-MacBook-Pro.local>
2023-01-11 00:30:09 -0300

    add sidebar

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,6 +4,18 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
+		{/* SideBar */}
+		<button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Destacados</button>
+
+		<div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+			<div className="offcanvas-header">
+				<h5 className="offcanvas-title" id="offcanvasScrollingLabel">Noticias destacadas</h5>
+				<button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+			</div>
+			<div className="offcanvas-body">
+				<p>Try scrolling the rest of the page to see this option in action.</p>
+			</div>
+		</div>
 		<h1>Aquí va el saludo de bienvenida</h1>
 		<p>
 			<img src={rigoImage} />
