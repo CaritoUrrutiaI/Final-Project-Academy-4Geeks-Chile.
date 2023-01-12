3756df49622b5a84a8f5426a886358874eae0c53
aramirezaliste <aramirezaliste@gmail.com>
2023-01-12 13:01:22 +0000

    Se agrega css a Cards

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
+import '../../styles/cards.css'
 
 export const CentralCard = () => {
     return (
 
-        <div className="card w-100" style={{"backgroundColor":"#D9D9D9", "color":"#796666","minHeight":"20rem"}}>
+        <div className="card w-100 cartas alturaMin" >
             <div className="card-body">
                 <h3 className="card-title text-center">Actividades</h3>
                 <p className="card-text ">Lista de actividades</p>
@@ -17,7 +18,7 @@ export const CentralCard = () => {
 export const FotoCard = () => {
     return (
 
-        <div className="card w-100 " style={{"backgroundColor":"#D9D9D9", "color":"#796666","minHeight":"20rem"}}>
+        <div className="card w-100 cartas" >
             <img src="https://cdn-icons-png.flaticon.com/512/6073/6073873.png" className=" card-img-top rounded-circle mx-auto position-absolute top-0 start-50 translate-middle" style={{"max-width": "10rem"}} alt="..." />
             <div className="card-body pt-5 mt-5">
                 <h5 className="card-title text-center">Nombre Usuario</h5>
@@ -32,7 +33,7 @@ export const FotoCard = () => {
 
 export const DestacadosCard = () => {
     return (
-        <div className="card w-100 " style={{"minHeight":"20rem" , "backgroundColor":"#D9D9D9", "color":"#796666"}}>
+        <div className="card w-100 cartas alturaMin">
             <div className="card-body text-center">
                 <h5 className="card-title">Destacados Usuario</h5>
                 
@@ -45,7 +46,7 @@ export const DestacadosCard = () => {
 
 export const DerechaCard = () => {
     return (
-        <div className="card w-100" style={{"backgroundColor":"#D9D9D9", "color":"#796666","minHeight":"20rem"}}>
+        <div className="card w-100 cartas alturaMin">
             <div className="card-body text-center">
                 <h5 className="card-title">Ventana Derecha</h5>
                 
