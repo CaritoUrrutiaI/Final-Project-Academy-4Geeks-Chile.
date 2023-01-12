b7fef9ca97a500c98b76afe423958afb8266ab8b
Carito 💫 <carito@Caritos-MacBook-Pro.local>
2023-01-12 01:59:36 -0300

    modify home.js

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<div className= "RR.SS">
-		<i class="fa-regular fa-envelope"></i>     <i class="fa-brands fa-instagram"></i>     <i class="fa-brands fa-facebook"></i>     <i class="fa-brands fa-linkedin"></i>
+		<i className="fa-regular fa-envelope"></i>     <i class="fa-brands fa-instagram"></i>     <i class="fa-brands fa-facebook"></i>     <i class="fa-brands fa-linkedin"></i>
 		</div>
 		<p>
 		<i className="fa-regular fa-copyright" />  Todos los derechos reservados by{" "}
