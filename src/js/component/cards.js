04163f0ee37c7ed65d4cbfeae9bd11ab629a184a
aramirezaliste <aramirezaliste@gmail.com>
2023-01-12 02:04:32 +0000

    Avance en View vistadeusuario

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -4,22 +4,54 @@ import React from 'react'
 export const CentralCard = () => {
     return (
 
-        <div className="card text-center">
-            <div className="card-header">
-                Actividades
-            </div>
+        <div className="card w-100">
             <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-            <div className="card-footer text-muted">
-                2 days ago
+                <h3 className="card-title text-center">Actividades</h3>
+                <p className="card-text text-center">Lista de actividades</p>
             </div>
         </div>
 
     )
 }
 
+export const FotoCard = () => {
+    return (
+
+        <div className="card w-100">
+            <img src="https://cdn-icons-png.flaticon.com/512/6073/6073873.png" className=" card-img-top rounded-circle mx-auto" style={{"max-width": "10em"}} alt="..." />
+            <div className="card-body">
+                <h5 className="card-title text-center">Nombre Usuario</h5>
+                <p className="card-text text-center">Santiago, Chile</p>
+            </div>
+        </div>
+
+    )
+
+}
 
 
+export const DestacadosCard = () => {
+    return (
+        <div className="card w-100 ">
+            <div className="card-body text-center">
+                <h5 className="card-title">Destacados Usuario</h5>
+                
+            </div>
+        </div>
+    )
+
+
+}
+
+export const DerechaCard = () => {
+    return (
+        <div className="card w-100">
+            <div className="card-body text-center">
+                <h5 className="card-title">Ventana Derecha</h5>
+                
+            </div>
+        </div>
+    )
+
+
+}
--- src/js/views/vistausuario.js
+++ src/js/views/vistausuario.js
@@ -1,21 +1,26 @@
 
 import propTypes from 'prop-types'
 import React from 'react'
-import { CentralCard } from '../component/cards'
+import { CentralCard, DerechaCard, DestacadosCard, FotoCard } from '../component/cards'
 
 
 export const VistaUsuario = (props) => {
     return (
-        <div className='row container-fluid'>
+        <div className='row container-fluid '>
             <h1>Vista del Usuario</h1>
-            <div className='col-4'>
-
+            <div className='col-3 d-flex flex-column pt-5 '>
+                <div className='mb-4'>
+                    <FotoCard />
+                </div>
+                <div>
+                    <DestacadosCard />
+                </div>
             </div>
-            <div className='col-4'>
-            <CentralCard/>
+            <div className='col-6 d-flex justify-content-center pt-5' >
+                <CentralCard />
             </div>
-            <div className='col-4'>
-
+            <div className='col-3 d-flex justify-content-center pt-5'>
+                <DerechaCard />
             </div>
 
 
