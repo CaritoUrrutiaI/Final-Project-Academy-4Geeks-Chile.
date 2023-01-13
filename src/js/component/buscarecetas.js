cd5371ee0e6d5cb8ffd0ac48830c66620a22c25d
Jaqa97 <joaquinonesa@gmail.com>
2023-01-13 00:44:57 +0000

    recetas2

--- src/js/component/buscarecetas.js
+++ src/js/component/buscarecetas.js
@@ -5,7 +5,7 @@ export const BuscadorRecetas = () => {
 	return (<div class="container text-center">
                 <form>
                     <div class="form-group my-5">
-                        <input type="search" class="form-control" placeholder="Busca tu receta"/>
+                        <input type="search" class="form-control" placeholder="Busca tu receta🔎"/>
                     </div>
                 </form>
             </div>)
