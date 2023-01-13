555d08e931201fc714b90b0c812ad13a66c83a44
Carito 💫 <carito@Caritos-MacBook-Pro.local>
2023-01-12 22:52:58 -0300

    wrapper

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,43 +3,53 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="container">
-		<div className="row">
-			{/* SideBar */}
-			<div className="col-8">
-			</div>
-			<div>
-			{/* Body Rigo */}
-			<div className="col-4 Body">
-				<h1>Aquí va el saludo de bienvenida</h1>
-				<p><img src={rigoImage} /></p>
-					<a href="#" className="btn btn-success">If you see this green button, bootstrap is working</a>
-			</div>
+	<div id="wrapper">
+		<div id="header">
+			<h1>Destacados</h1>
 		</div>
-		</div>
-				{/* <!-- Card para información  --> */}
-		<div class="card-group">
-				<div class="card">
-					<img src="..." class="card-img-top" alt="..." />
-					<div class="card-body">
-						<h5 class="card-title">1era Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-					</div>
-				</div>
-				<div class="card">
-					<img src="..." class="card-img-top" alt="..." />
-					<div class="card-body">
-						<h5 class="card-title">2da Card title</h5>
-						<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-					</div>
-				</div>
-				<div class="card">
-					<img src="..." class="card-img-top" alt="..." />
-					<div class="card-body">
-						<h5 class="card-title">3era Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-					</div>
-				</div>
-		</div>
+		<div id="nav1"><h3>What is Lorem Ipsum?</h3></div>
+		<div id="content"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p></div>
+		<div id="nav2"><h2>subdirectory</h2></div>
+		<div id="content"><p>Lorem ipsus blablabkkajajaakakjakjakaajajkajajk</p></div>
 	</div>
+	// <div className="container-fluid mt-3">
+	// 	<div className="row">
+	// 		{/* SideBar */}
+	// 		<div className="col-sm-4 bg-primary text-white">
+	// 			<h1>Prueba</h1>
+	// 			<p><img src={rigoImage} /></p>
+	// 		</div>
+	// 		<div>
+	// 		{/* Body Rigo */}
+	// 		<div className="col-sm-8 bg-dark text-white">
+	// 			<h1>Aquí va el saludo de bienvenida</h1>
+	// 			<p><img src={rigoImage} /></p>
+	// 		</div>
+	// 	</div>
+	// 	</div>
+	// 			{/* <!-- Card para información  --> */}
+	// 	{/* <div class="card-group">
+	// 			<div class="card">
+	// 				<img src="..." class="card-img-top" alt="..." />
+	// 				<div class="card-body">
+	// 					<h5 class="card-title">1era Card title</h5>
+	// 					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	// 				</div>
+	// 			</div>
+	// 			<div class="card">
+	// 				<img src="..." class="card-img-top" alt="..." />
+	// 				<div class="card-body">
+	// 					<h5 class="card-title">2da Card title</h5>
+	// 					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+	// 				</div>
+	// 			</div>
+	// 			<div class="card">
+	// 				<img src="..." class="card-img-top" alt="..." />
+	// 				<div class="card-body">
+	// 					<h5 class="card-title">3era Card title</h5>
+	// 					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+	// 				</div>
+	// 			</div>
+	// 	</div> */}
+	// </div>
 );
