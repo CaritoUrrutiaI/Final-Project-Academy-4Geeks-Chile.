80e0ea62172bb4f3eef06dd0f7f21af1c40a6feb
Carito 💫 <carito@Caritos-MacBook-Pro.local>
2023-01-18 13:04:34 -0300

    add blue line

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,57 +4,59 @@ import '../../styles/navbar.css'
 
 export const Navbar = () => {
 	return (
+		<div>
+			<nav className="navbar navbar-expand-lg  navbarColor textoTamañoMediano px-0 ">
+				<nav className="navbar ps-3">
+					<div className="container-fluid">
+						<Link to="/" className="text-decoration-none">
+							<i className="fa-solid fa-person-biking  navbarFontColor textoTamañoGrande"></i><a className="navbar-brand ps-2 navbarFontColor textoTamañoGrande" href="#">BIENESTAR</a>
+						</Link>
+					</div>
+				</nav>
+				<ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+					<li className="nav-item">
+						<Link to="/recetas" className="text-decoration-none">
+							<a className="nav-link active navbarFontColor" aria-current="page" href="#">Recetas</a>
+						</Link>
+					</li>
+					<li className="nav-item">
 
-		<nav className="navbar navbar-expand-lg  navbarColor textoTamañoMediano px-0 ">
-			<nav className="navbar ps-3">
-				<div className="container-fluid">
-					<Link to="/" className="text-decoration-none">
-						<i className="fa-solid fa-person-biking  navbarFontColor textoTamañoGrande"></i><a className="navbar-brand ps-2 navbarFontColor textoTamañoGrande" href="#">BIENESTAR</a>
+						<a className="nav-link navbarFontColor" href="#">Deportes</a>
+
+					</li>
+					<li className="nav-item">
+						<Link to="/tusemana" className="text-decoration-none">
+							<a className="nav-link navbarFontColor" href="#">Tu semana</a>
+						</Link>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link navbarFontColor" href="#">Objetivos</a>
+					</li>
+				</ul>
+				{/* login icon */}
+				<li className="navbar-nav px-1">
+					<Link to="/vistausuario" className="text-decoration-none">
+						<i className="fa-regular fa-circle-user  navbarFontColor textoTamañoGrande"></i>
 					</Link>
-				</div>
+				</li>
+
+				<li className="navbar-nav pe-3 ps-1">
+					<Link to="/login" className="text-decoration-none">
+						<a className="nav-link  navbarFontColor" href="#">Login</a>
+					</Link>
+					<Link to="/signup" className="text-decoration-none">
+						<a className="nav-link  navbarFontColor" href="#">Signup</a>
+					</Link>
+				</li>
+				{/* logout icon */}
+				<li className="navbar-nav px-3">
+					<Link to="/logout" className="text-decoration-none">
+						<i class="fa-solid fa-arrow-right-from-bracket tamañotextoGrande"></i>
+					</Link>
+				</li>
 			</nav>
-			<ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-				<li className="nav-item">
-					<Link to="/recetas" className="text-decoration-none">
-						<a className="nav-link active navbarFontColor" aria-current="page" href="#">Recetas</a>
-					</Link>
-				</li>
-				<li className="nav-item">
-
-					<a className="nav-link navbarFontColor" href="#">Deportes</a>
-
-				</li>
-				<li className="nav-item">
-					<Link to="/tusemana" className="text-decoration-none">
-						<a className="nav-link navbarFontColor" href="#">Tu semana</a>
-					</Link>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link navbarFontColor" href="#">Objetivos</a>
-				</li>
-			</ul>
-			{/* login icon */}
-			<li className="navbar-nav px-1">
-				<Link to="/vistausuario" className="text-decoration-none">
-					<i className="fa-regular fa-circle-user  navbarFontColor textoTamañoGrande"></i>
-				</Link>
-			</li>
-
-			<li className="navbar-nav pe-3 ps-1">
-				<Link to="/login" className="text-decoration-none">
-					<a className="nav-link  navbarFontColor" href="#">Login</a>
-				</Link>
-				<Link to="/signup" className="text-decoration-none">
-					<a className="nav-link  navbarFontColor" href="#">Signup</a>
-				</Link>
-			</li>
-            {/* logout icon */}
-			<li className="navbar-nav px-3">
-				<Link to="/logout" className="text-decoration-none">
-				<i class="fa-solid fa-arrow-right-from-bracket tamañotextoGrande"></i>
-				</Link>
-			</li>
-		</nav>
+			<div class="linea-azul-navbar"></div>
+		</div>
 	);
 };
 
