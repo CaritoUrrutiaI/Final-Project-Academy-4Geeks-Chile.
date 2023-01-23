1b08fc968dad3677f6ea7d6280090d4bcd0de1fd
jaranedag <jaranedag@udd.cl>
2023-01-23 23:41:53 +0000

    autenticacion login

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -41,11 +41,22 @@ const getState = ({
 				};
 
 				fetch("https://3000-4geeksacade-flaskresthe-nxl0tay09lo.ws-us83.gitpod.io/user", requestOptions)
-					.then(response => response.text())
-					.then(result => {setStore({user:result})
+					.then(response => response.json())
+					.then(result => {
+					console.log(result)	
+					if(result.token){
+
+					
+					setStore({user:result})
+					
 						alert("ingreso completado")
 						window.location.href ="/";
-					})
+					}
+					else{
+						alert("error")
+					}}
+					
+					)
 					.catch(error => console.log('error', error));
 			},
 			signup: (username, email, name, apellido, contraseña) => {
